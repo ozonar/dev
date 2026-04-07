d2c18eee5f56010473d52a6b8735404dacbdbd22
ozonar <ozon@r>
2026-04-07 18:36:31 +0700

    feat: version from tag

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version текущая версия программы dev.
 // Может быть переопределена при сборке через -ldflags "-X dev/internal/version.Version=..."
-var Version = "1.0.5"
+var Version = "wrong-build"
