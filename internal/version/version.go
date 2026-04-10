73761cf33e70563105e73c1a6b792da449a657c7
ozonar <ozon@r>
2026-04-10 15:19:00 +0700

    fix: название билда

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version текущая версия программы dev.
 // Может быть переопределена при сборке через -ldflags "-X dev/internal/version.Version=..."
-var Version = "wrong-build"
+var Version = "local-build"
